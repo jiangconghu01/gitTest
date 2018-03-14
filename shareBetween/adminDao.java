e3bbb2178fa2403abab9e18a2580801aad2674bd
jiangconghu01 <1239395043@qq.com>
2018-03-14 11:06:52 +0800

    Update adminDao.java

--- shareBetween/adminDao.java
+++ shareBetween/adminDao.java
@@ -1,1 +1,12 @@
-wrewerw
+package com.huawei.logpro.dao;
+
+import java.util.Map;
+
+import com.huawei.logpro.entity.Admin;
+
+public interface AdminDao {
+	public void saveAdmin(Admin admin);
+	public Admin findAdminByName(String name);
+	public int updateAdmin(Map<String,Object> map);
+	public int modMessage(Map<String,Object> map);
+}
