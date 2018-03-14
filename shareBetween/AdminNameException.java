928ad6858ae75f70ba69cf553dc4f5ba1f4f585c
jiangconghu01 <1239395043@qq.com>
2018-03-14 11:18:06 +0800

    Create AdminNameException.java

---
+++ shareBetween/AdminNameException.java
@@ -0,0 +1,37 @@
+package com.huawei.logpro.service;
+
+public class AdminNameException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AdminNameException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNameException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNameException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNameException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNameException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
