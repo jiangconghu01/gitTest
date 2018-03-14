cc251314e8015c5dfaf038cf5a9265618dc3fd43
jiangconghu01 <1239395043@qq.com>
2018-03-14 11:16:01 +0800

    Create ConstantCollection.java

---
+++ shareBetween/ConstantCollection.java
@@ -0,0 +1,18 @@
+package com.huawei.logpro.entity;
+
+public class ConstantCollection {
+	public static final String NULL_NAME="用户名不能为空";
+	public static final String NULL_PASSWORD="密码不能为空";
+	public static final String ERROR_PASSWORD="密码错误";
+	public static final String NULL_ADMIN="用户不存在";
+	public static final String ERROR_VERIFICATION_CODE="验证码错误";
+	public static final String NOFIT_NAME="用户名不合规格";
+	public static final String NOFIT_PASSWORD="密码不合规格";
+	public static final String NOFIT_CONFRIMPASS="密码不合规格";
+	public static final String REGISTER="用户已经注册";
+	public static final String IMGCODE_NULL="验证码为空";
+	public static final String IMGCODE_ERROR="验证码错误";
+	public static final String SUCCESS="success";
+	public static final String FAILED="failed";
+	public static final String NOT_LOGIN="请重新登录";
+}
