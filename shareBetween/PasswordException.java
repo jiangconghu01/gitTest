54aaabd7b061f903c39fec44d098b615a00d7467
jiangconghu01 <1239395043@qq.com>
2018-03-14 11:23:16 +0800

    Create PasswordException.java

---
+++ shareBetween/PasswordException.java
@@ -0,0 +1,35 @@
+package com.huawei.logpro.service;
+
+public class PasswordException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PasswordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
