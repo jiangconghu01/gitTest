122ec20549d3910f77eab1a5a87cb6251a8dcd0c
jiangconghu01 <1239395043@qq.com>
2018-03-14 11:19:02 +0800

    Create AdminNotFoundException.java

---
+++ shareBetween/AdminNotFoundException.java
@@ -0,0 +1,35 @@
+package com.huawei.logpro.service;
+
+public class AdminNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AdminNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminNotFoundException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
