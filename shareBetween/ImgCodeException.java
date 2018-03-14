652bcb7009613ee98eadffb911c6ccbda2b76629
jiangconghu01 <1239395043@qq.com>
2018-03-14 11:22:41 +0800

    Create ImgCodeException.java

---
+++ shareBetween/ImgCodeException.java
@@ -0,0 +1,36 @@
+package com.huawei.logpro.service;
+
+public class ImgCodeException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ImgCodeException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImgCodeException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImgCodeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImgCodeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImgCodeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
