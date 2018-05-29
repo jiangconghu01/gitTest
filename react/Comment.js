9aef3368ee5a8b6924677d3d1cf550242ebe181e
jiangconghu01 <1239395043@qq.com>
2018-05-29 17:23:02 +0800

    Create Comment.js

---
+++ react/Comment.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class Comment extends Component{
+    render(){
+        return (
+            <div className="comment-box">
+                <span className="user-name" title={this.props.comment.userName}>{this.props.comment.userName}:</span>
+                <span className="user-content" title={this.props.comment.content}>{this.props.comment.content}</span>
+            </div>
+        );
+    }
+}
+export default Comment;
